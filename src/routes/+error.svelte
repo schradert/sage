3ae7e370cf4bc6a0b8ead746abf0e817c8b040df
Tristan Schrader <t0rdos@pm.me>
2024-05-12 13:37:31 -0700

    feat: initialize repo
    1. sveltekit with xyflow
    2. canivete with k3d kubenix (supabase + penpot) [wip]

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import { page } from '$app/stores'
+    import { emojis } from '$lib/emojis.ts'
+</script>
+
+<h1>{$page.status} {$page.error.message}</h1>
+<span>{emojis[$page.status] ?? emojis[500]}</span>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import '../app.css'
+    import { ModeWatcher, toggleMode } from 'mode-watcher'
+    import Sun from 'lucide-svelte/icons/sun'
+    import Moon from 'lucide-svelte/icons/moon'
+    import { Button } from '$lib/components/ui/button'
+    import * as Menubar from '$lib/components/ui/menubar'
+</script>
+
+<ModeWatcher />
+<Menubar.Root>
+    <Menubar.Menu>
+        <Button on:click={toggleMode} variant="outline" size="icon">
+            <Sun class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <span class="sr-only">Toggle theme</span>
+        </Button>
+    </Menubar.Menu>
+</Menubar.Root>
+<slot />
