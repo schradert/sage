1aaa6f52977357896523a36be1c2953d073773e6
Tristan Schrader <tristan@climaxfoods.com>
2024-05-13 13:50:44 -0700

    refactor: remove unused files/libs, migrate to biome

--- flake.nix
+++ flake.nix
@@ -1,11 +1,5 @@
 {
-  inputs = {
-    canivete.url = github:schradert/canivete;
-    supabase.url = github:supabase-community/supabase-kubernetes;
-    supabase.flake = false;
-    penpot.url = github:codechem/helm;
-    penpot.flake = false;
-  };
+  inputs.canivete.url = github:schradert/canivete;
   outputs = inputs:
     inputs.canivete.lib.mkFlake {inherit inputs;} {
       perSystem = {pkgs, ...}: {
@@ -16,13 +10,6 @@
             languages.javascript.enable = true;
             # Remap sveltekit assets to correct folder for static link checking
             settings.hooks.lychee.settings.flags = "--remap 'src/%25sveltekit.assets%25 static'";
-          };
-          kubenix.clusters.local = {
-            k3d = true;
-            modules.main.kubernetes.helm.releases = {
-              supabase.chart = "${inputs.supabase}/charts/supabase";
-              penpot.chart = "${inputs.penpot}/charts/penpot";
-            };
           };
         };
       };
--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -1,32 +1,31 @@
-const db = new Map();
+const db = new Map()
 
 export function getTodos(userid) {
-	if (!db.has(userid))
-		createUpdateTodo({ userid, description: "Learn about API routes" });
-	return Array.from(db.get(userid).values());
+  if (!db.has(userid)) createUpdateTodo({ userid, description: "Learn about API routes" })
+  return Array.from(db.get(userid).values())
 }
 
 export function createUpdateTodo({ userid, description }) {
-	if (!db.has(userid)) db.set(userid, new Map());
-	if (description === "") throw new Error("todo must have a description");
+  if (!db.has(userid)) db.set(userid, new Map())
+  if (description === "") throw new Error("todo must have a description")
 
-	const todos = db.get(userid);
+  const todos = db.get(userid)
 
-	todos.set(userid, {
-		id: crypto.randomUUID(),
-		description,
-		done: false,
-	});
+  todos.set(userid, {
+    id: crypto.randomUUID(),
+    description,
+    done: false
+  })
 
-	return { userid };
+  return { userid }
 }
 
 export function toggleTodo({ userid, id, done }) {
-	const todos = db.get(userid);
-	todos.get(id).done = done;
+  const todos = db.get(userid)
+  todos.get(id).done = done
 }
 
 export function deleteTodo({ userid, id }) {
-	const todos = db.get(userid);
-	todos.delete(id);
+  const todos = db.get(userid)
+  todos.delete(id)
 }
