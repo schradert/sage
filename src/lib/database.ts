e4135ca0555b461d69e6df7d808cfc9311a0ad70
Tristan Schrader <t0rdos@pm.me>
2024-05-24 12:56:24 -0700

    feat: simple Graph model definition
    it has edges, nodes, name, and ID, and I overdid it with this TypeScript nonsense

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -2,6 +2,53 @@ import { type Edge, type Node } from "@xyflow/svelte"
 // TODO convert to persisted
 import { persisted } from "svelte-persisted-store"
 import { derived } from "svelte/store"
+import { v4 as uuidv4 } from "uuid"
+
+// Taken from https://stackoverflow.com/a/64099424
+
+// Build type Type with arguments Args
+type Constructor<Type, Args extends unknown[]> = new (...args: Args) => Type
+// Exclude first element of array
+type Tail<Args extends readonly unknown[]> = Args extends [unknown, ...infer Rest] ? Rest : never
+
+type Name = string
+interface Named {
+    name: Name
+}
+type AddNamed<Args extends unknown[]> = Args extends [] ? [Named] : [Args[0] & Named, ...Tail<Args>]
+const NamedMixin = <Base extends Constructor<any, any[]>>(base: Base): Constructor<
+    InstanceType<Base> & Named,
+    AddNamed<ConstructorParameters<Base>>
+> => {
+    return class New extends base {
+        public name: Name
+        constructor (...args: any[]) {
+            super(...args)
+            const { name } = args[0] as Named
+            this.name = name
+        }
+    }
+}
+
+type UUID = string
+class Unique {
+    id: UUID
+    constructor() {
+        this.id = uuidv4()
+    }
+}
+
+class Graph extends NamedMixin(Unique) {
+    public nodes: Node[]
+    public edges: Edge[]
+    constructor (...args: any[]) {
+        // TODO why do I have to be explicit here?
+        super(...args as [Named])
+        const { nodes, edges } = args[0] as { nodes: Node[]; edges: Edge[] }
+        this.nodes = nodes ?? []
+        this.edges = edges ?? []
+    }
+}
 
 const _nodes: Node[] = [
     //  {
